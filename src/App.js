454a933ddc7f869cd43a0bc935dd74082a1a2bd5
John Pham <hprobotic@gmail.com>
2018-03-18 21:20:46 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HomePage, UserInfo } from './pages';
-
 import './App.css';
 
 class App extends Component {
